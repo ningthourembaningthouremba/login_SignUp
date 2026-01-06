a0ebf3256de03a1901b775adeddb0f5774066bca
Ningthouremba <ningthouremba@gmail.com>
2026-01-06 10:37:21 +0530

    thirdround

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -1,32 +1,21 @@
-import nodemailer from "nodemailer";
-import dotenv from "dotenv";
-dotenv.config();
-
+import nodemailer from "nodemailer"
+import dotenv from 'dotenv';
+dotenv.config()
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
+  service: "gmail", // or your email provider
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
 });
-
-
 export const sendEmail = async (to, subject, html) => {
-  console.log(to);
-  console.log(process.env.EMAIL_PASS);
-  console.log(process.env.EMAIL_USER);
-
- 
   try {
     const info = await transporter.sendMail({
       from: process.env.EMAIL_USER,
-      to,           // ✅ dynamic
-      subject,      // ✅ dynamic
-      html,         // ✅ dynamic
+      to,
+      subject,
+      html,
     });
-
     console.log("Email sent:", info.messageId);
   } catch (error) {
     console.error("Email sending error:", error);
